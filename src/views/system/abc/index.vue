58547b1837e436b9908074195e1a66f7f55d4185
zwf <417610688@qq.com>
2020-10-12 19:13:55 +0800

    添加abc表单

---
+++ src/views/system/abc/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <div v-if="crud.props.searchToggle">
+        <!-- 搜索 -->
+        <label class="el-form-item-label">id</label>
+        <el-input v-model="query.id" clearable placeholder="id" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">field1</label>
+        <el-input v-model="query.field1" clearable placeholder="field1" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">createTime</label>
+        <el-input v-model="query.createTime" clearable placeholder="createTime" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <rrOperation :crud="crud" />
+      </div>
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="field1" prop="field1">
+            <el-input v-model="form.field1" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="field2">
+            <el-input v-model="form.field2" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="createTime">
+            <el-input v-model="form.createTime" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="field1" label="field1">
+          <template slot-scope="scope">
+            {{ dict.label.user_status[scope.row.field1] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="field2" label="field2" />
+        <el-table-column prop="createTime" label="createTime">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-permission="['admin','abc:edit','abc:del']" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudAbc from '@/api/system/abc'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { id: null, field1: null, field2: null, createTime: null }
+export default {
+  name: 'Abc',
+  components: { pagination, crudOperation, rrOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  dicts: ['user_status'],
+  cruds() {
+    return CRUD({ title: 'api/abc', url: 'api/abc', idField: 'id', sort: 'id,desc', crudMethod: { ...crudAbc }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'abc:add'],
+        edit: ['admin', 'abc:edit'],
+        del: ['admin', 'abc:del']
+      },
+      rules: {
+        field1: [
+          { required: true, message: '不能为空', trigger: 'blur' }
+        ]
+      },
+      queryTypeOptions: [
+        { key: 'id', display_name: 'id' },
+        { key: 'field1', display_name: 'field1' },
+        { key: 'createTime', display_name: 'createTime' }
+      ]
+    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
